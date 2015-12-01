e82fdc64db8550031463d0cb127027a92d83bc77
ericnam808 <ericnam808@gmail.com>
2015-11-30 22:38:22 -0800

    Initial word count in EmailClassifier

--- nb-email-classifier/src/main/java/edu/sjsu/cs286/emailcf/spark/EmailClassifier.java
+++ nb-email-classifier/src/main/java/edu/sjsu/cs286/emailcf/spark/EmailClassifier.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 
 import edu.sjsu.cs286.wordcount.SparkWordCount;
@@ -27,28 +29,59 @@ public class EmailClassifier {
         JavaPairRDD<String, List<String>> rdd = sc.textFile(myTextFile).mapToPair(
             new PairFunction<String, String, List<String>>() {
                 public Tuple2<String, List<String>> call(String line) {
-                    String[] csValues = line.toLowerCase().replaceAll("[\"]", "").split(",");
+                    String[] csValues = line.toLowerCase().replaceAll("[\"]", "").replaceAll("\\s+", ",").split(",");
                     List<String> words = Arrays.asList(Arrays.copyOfRange(csValues, 1, csValues.length));
                     return new Tuple2<String, List<String>>(csValues[0], words);  
                 }
             });
 
+        // Call WordCount
+		// Extract the string lines
+        
+		JavaRDD<String> lines = rdd.flatMap(
+            new FlatMapFunction<Tuple2<String, List<String>>, String>() {
+
+					public Iterable<String> call(Tuple2<String, List<String>> t) {
+						return t._2;
+					}
+            });
+
+		// Turn the words into (word, 1) pairs
+		JavaPairRDD<String, Integer> wordOnePairs = lines
+				.mapToPair(new PairFunction<String, String, Integer>() {
+
+					public Tuple2<String, Integer> call(String x) {
+						return new Tuple2<String, Integer>(x, 1);
+					}
+				});
+		
+		// reduce add the pairs by key to produce counts
+		JavaPairRDD<String, Integer> counts = wordOnePairs
+				.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+					public Integer call(Integer x, Integer y) {
+						return x + y;
+					}
+				});
+                
         /* Split data as testing and training */
         // 70% is training, 30% is testing data
         double trainingRatio = 0.7;
+        
+        
         /* Training */
         // Create model
         
-        // Call WordCount
+
         
         /* Testing */
         // Test the classifier
         
         /* Confusion Matrix*/
 
-            System.out.println("Precision: ");
-            System.out.println("Recall: ");
-            System.out.println("Accuracy: ");
-        
+        System.out.println("Precision: ");
+        System.out.println("Recall: ");
+        System.out.println("Accuracy: ");
+        counts.saveAsTextFile("/user/user01/cs286-spark-nb-email-classification/output/counts");
     }
 }
